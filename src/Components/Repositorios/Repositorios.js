90e81ea61e304c8995c997bc6a2b61b16fde055b
LipzDev <filipe.gt1998@outlook.com>
2021-05-06 01:44:46 -0300

    Arquivos

--- src/Components/Repositorios/Repositorios.js
+++ src/Components/Repositorios/Repositorios.js
@@ -3,6 +3,19 @@ import { Helmet } from 'react-helmet';
 import './Repositorios.css';
 
 const Repositorios = () => {
+  const [item, setItem] = React.useState(null);
+
+  React.useEffect(() => {
+    async function ApiRequest() {
+      const response = await fetch(
+        `https://api.github.com/users/LipzDev/repos`,
+      );
+      const json = await response.json();
+      setItem(json);
+    }
+    ApiRequest();
+  }, []);
+
   return (
     <section className="repositorios  efeito__transicao">
       <Helmet>
@@ -10,9 +23,22 @@ const Repositorios = () => {
         <meta name="description" content="Página de repositórios do github." />
         <title>Repositórios</title>
       </Helmet>
-      <div className="container">
+      <div className="container-w2">
         <div className="repositorios__item">
-          <h1>REPOSITORIO</h1>
+          {item &&
+            item.map(
+              (item, index, arr) =>
+                index >=
+                  20 /*NECESSÁRIO CRIAR UMA LOGICA PARA EXIBIR APENAS OS ITENS RECENTES */ &&
+                !item.fork && (
+                  <div className="repositorios__box" key={item.id}>
+                    <h3>{item.name}</h3>
+                    <p>{item.description}</p>
+                    <a href={item.html_url}>Visitar</a>
+                    {(arr, index)}
+                  </div>
+                ),
+            )}
         </div>
       </div>
     </section>
