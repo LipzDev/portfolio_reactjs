69dbedabab714ce80284782d27dda2494ffff38e
LipzDev <filipe.gt1998@outlook.com>
2021-05-16 20:21:49 -0300

    Página contato

--- src/Components/Contato/Contato.js
+++ src/Components/Contato/Contato.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import icon1 from './Assets/face.png';
+import icon2 from './Assets/wpp1.png';
+import icon3 from './Assets/lkd.png';
+import icon4 from './Assets/github.png';
 import './Contato.css';
 
 const Contato = () => {
@@ -12,7 +16,22 @@ const Contato = () => {
       </Helmet>
       <div className="container">
         <div className="contato__item">
-          <h1>CONTATO</h1>
+          <a href="#">
+            <img src={icon1} alt="Icone do Facebook" />
+            <p>Facebook</p>
+          </a>
+          <a href="#">
+            <img src={icon2} alt="Icone do Whatsapp" />
+            <p>Whatsapp</p>
+          </a>
+          <a href="#" className="efeito__icon">
+            <img src={icon3} alt="Icone do LinkedIn" />
+            <p>LinkedIn</p>
+          </a>
+          <a href="#" className="efeito__icon">
+            <img src={icon4} alt="Icone do Github" />
+            <p>GitHub</p>
+          </a>
         </div>
       </div>
     </section>
