fccf394dfb2a84b59f6cdf4ddada2bcf2f773d38
LipzDev <filipe.gt1998@outlook.com>
2021-05-17 23:57:43 -0300

    Mudanças pagina contato

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Projetos from './Components/Projetos/Projetos';
 import Repositorios from './Components/Repositorios/Repositorios';
 import Contato from './Components/Contato/Contato';
 import Particles from './Components/Particles/Particles';
+import Error from './Components/Error/Error';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="projetos" element={<Projetos />} />
           <Route path="repositorios" element={<Repositorios />} />
           <Route path="contato" element={<Contato />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </>
